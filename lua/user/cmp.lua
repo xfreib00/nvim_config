b1658e9f7ec063a4a44bcbff783a1024e76414fa
Adam Freiberg <xfreib00@stud.fit.vutbr.cz>
2022-10-25 22:46:43 +0200

    [BUGFIX] Fix tab not working in insert mode
    Fix missing function in cmp file preventing correct function of tab key
    in insert mode.

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -8,6 +8,11 @@ if not snip_status_ok then
   return
 end
 
+local check_backspace = function()
+  local col = vim.fn.col "." - 1
+  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+end
+
 require("luasnip/loaders/from_vscode").lazy_load()
 
 local kind_icons = {
